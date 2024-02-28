5148e6391f8b165b39a112493c1d0f6fc3c0d226
aquaGGgg <158755130+aquaGGgg@users.noreply.github.com>
2024-02-28 17:12:25 +0300

    asddasasdasdasd

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    asdasdasd
+    asdasdasdasdasdasdasdasdasdas
   </div>
 );
 
